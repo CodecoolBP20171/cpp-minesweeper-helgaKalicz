74a24211a2d3ec4b5d547dcf433b874a16f570bf
helgaKalicz <kalicz.helga@gmail.com>
2017-10-04 10:58:59 +0200

    fillTable and printTable are ready

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 namespace {
     class Minesweeper {
@@ -23,15 +24,35 @@ namespace {
 
         void countNeighbours() {
             // step 2 goes here
+            for (int i = 0; i < height*width; ++i) {
+                for (int j = 0; j < 8; ++j) {
+
+                }
+            }
         }
 
         void printTable() const {
             // step 3 goes here
+            int counter = 0;
+            for (int i = 0; i < height; ++i) {
+                for (int j = 0; j < width*4+1; ++j) std::cout << '-';
+                std::cout << std::endl;
+                for (int k = 0; k < width; ++k) {
+                    std::cout << "| " << table[counter] << " ";
+                    ++counter;
+                }
+                std::cout << "|" << std::endl;
+            }
+            for (int j = 0; j < width*4+1; ++j) std::cout << '-';
+            std::cout << std::endl;
         }
 
     private:
         void fillTable() {
             // step 1 goes here
+            for (int i = 0; i < height*width; ++i) {
+                rand() % 10 > 7 ? table[i] = '*' : table[i] = '.';
+            }
         }
 
         const size_t width, height;
@@ -41,7 +62,7 @@ namespace {
 
 int main() {
     try {
-        Minesweeper ms(100, 50);
+        Minesweeper ms(5, 5);
         ms.printTable();
         ms.countNeighbours();
         ms.printTable();
