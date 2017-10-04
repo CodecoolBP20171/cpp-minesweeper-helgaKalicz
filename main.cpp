1cceeb4bacb6c9193123af4fd6ca04ae2a099109
helgaKalicz <kalicz.helga@gmail.com>
2017-10-04 11:05:47 +0200

    placing mines is different

--- main.cpp
+++ main.cpp
@@ -23,42 +23,8 @@ namespace {
             delete[] table;
         }
 
-        void printTable() const {
-            // step 3 goes here
-            int fieldWidthSize = 0;
-            for (std::vector<Mine> fieldRow : mineField) {
-                fieldWidthSize = fieldRow.size();
-                // Print a line above the rows
-                for (int i = 0; i < fieldWidthSize*4+1; ++i) {
-                    std::cout << '-';
-                }
-                std::cout << std::endl;
-                for (Mine fieldCell : fieldRow) {
-                    std::cout << "| ";
-                    fieldCell.getIsMine() ? std::cout << '*' : std::cout << fieldCell.getNeighbours();
-                    std::cout << " ";
-                }
-                std::cout << "|" << std::endl;
-            }
-            // Print a line under the last row
-            for (int i = 0; i < fieldWidthSize*4+1; ++i) {
-                std::cout << '-';
-            }
-        }
-
-    private:
-
-        void generateRandomMines() {
-            while (true) {
-                int randomMine = rand() % (height * width);
-                if (!(find(mineIndex.begin(), mineIndex.end(), randomMine) != mineIndex.end())) mineIndex.push_back(randomMine);
-                if (mineIndex.size() == mineNumber) break;
-            }
-            std::sort(mineIndex.begin(), mineIndex.end());
-        }
-
-        // step 2 goes here
         void countNeighbours() {
+            // step 2 goes here
             for (int i = 0; i < height; ++i) {
                 for (int j = 0; j < width; ++j) {
                     if (mineField[i][j].getIsMine()) {
@@ -66,7 +32,6 @@ namespace {
                             for (int l = -1; l <= 1; ++l) {
                                 if (!(k == 0 && l == 0)) {
                                     if (!(i+k < 0 || i+k >= height || j+l < 0 || j+l >= width)) {
-
                                         mineField[i+k][j+l].addNeighbours();
                                     }
                                 }
@@ -77,31 +42,40 @@ namespace {
             }
         }
 
-        // step 1 goes here
-        void fillTable() {
-            generateRandomMines();
-            int counter = 0;
-            for (int i = 0; i < height; ++i) {
-                std::vector<Mine> cellCharacter;
-                for (int j = 0; j < width; ++j) {
-                    if (mineIndex[0] == counter) {
-                        mineIndex.erase(mineIndex.begin());
-                        cellCharacter.push_back(Mine(true));
-                    } else {
-                        cellCharacter.push_back(Mine(false));
-                    }
-                    ++counter;
+        void printTable() const {
+            // step 3 goes here
+            int fieldWidthSize = 0;
+            for (std::vector<Mine> fieldRow : mineField) {
+                fieldWidthSize = fieldRow.size();
+                // Print a line above the rows
+                for (int i = 0; i < fieldWidthSize*4+1; ++i) std::cout << '-';
+                std::cout << std::endl;
+                for (Mine fieldCell : fieldRow) {
+                    std::cout << "| ";
+                    fieldCell.getIsMine() ? std::cout << '*' : std::cout << fieldCell.getNeighbours();
+                    std::cout << " ";
                 }
-                mineField.push_back(cellCharacter);
+                std::cout << "|" << std::endl;
             }
-            countNeighbours();
+            // Print a line under the last row
+            for (int i = 0; i < fieldWidthSize*4+1; ++i) std::cout << '-';
+        }
+
+    private:
+
+        void fillTable() {
+            // step 1 goes here
+            for (int i = 0; i < height; ++i) {
+                std::vector<Mine> character;
+                for (int j = 0; j < width; ++j) {
+                    rand() % 10 > 7 ? character.push_back(Mine(true)) : character.push_back(Mine(false));
+                }
+            }
         }
 
         const size_t width, height;
         char *table;
         std::vector<std::vector<Mine>> mineField;
-        std::vector<int> mineIndex;
-        const int mineNumber = 10;
     };
 }
 
@@ -109,6 +83,8 @@ int main() {
     srand(time(0));
     try {
         Minesweeper ms(5, 5);
+        ms.printTable();
+        ms.countNeighbours();
         ms.printTable();
     } catch (const std::bad_alloc &e) {
         std::cerr << "Couldn't allocate enough memory for minesweeper table" << std::endl;
